565a1150a88932490b75c99c52fecfac2dd7077d
Andrzejf <afornalik2@gmail.com>
2019-11-10 14:55:53 +0100

    Add implementation of shuffleCard() method

--- src/main/java/entity/deck/Deck.java
+++ src/main/java/entity/deck/Deck.java
@@ -1,16 +1,22 @@
 package entity.deck;
 
 import entity.cardtypes.Card;
+import service.ICardService;
 
 
-
+import java.util.Collections;
 import java.util.List;
 
-public abstract class Deck{
+public abstract class Deck implements ICardService {
 
     private List<Card> deckOfCards;
 
     public List<Card> getDeckOfCards() {
         return deckOfCards;
+    }
+
+    @Override
+    public void shuffleCard() {
+        Collections.shuffle(deckOfCards);
     }
 }
