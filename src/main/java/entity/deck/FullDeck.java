024c76bcf5e786ab671c937a008c489cb5287f1d
Andrzejf <afornalik2@gmail.com>
2019-11-10 15:20:35 +0100

    Create class to create Deck of initial state.

--- src/main/java/entity/deck/FullDeck.java
+++ src/main/java/entity/deck/FullDeck.java
@@ -1,19 +1,17 @@
 package entity.deck;
 
 import entity.Player;
-import entity.cardtypes.Card;
+
 import service.IInitialDeckService;
 
-import java.util.List;
+
 
 public abstract class FullDeck extends Deck implements IInitialDeckService{
 
     private final Player[] players;
-    private final int NUMBER_OF_CARDS_PER_PLAYER;
 
-    public FullDeck(List<Card> deckOfCards, Player[] players) {
+    public FullDeck( Player[] players) {
         this.players = players;
-        this.NUMBER_OF_CARDS_PER_PLAYER = super.getDeckOfCards().size() / players.length;
     }
 
 }
---
+++ src/main/java/entity/deck/implementation/FullDeckInitialState.java
@@ -0,0 +1,22 @@
+package entity.deck.implementation;
+
+import entity.Player;
+import entity.deck.FullDeck;
+
+
+public class FullDeckInitialState extends FullDeck {
+
+    public FullDeckInitialState( Player[] players) {
+        super(players);
+    }
+
+    @Override
+    public void createFullDeckOfCards() {
+
+    }
+
+    @Override
+    public void splitFullDeckAmongPlayers() {
+
+    }
+}
