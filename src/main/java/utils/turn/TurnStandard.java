6ae1043b559927f463f923e40681cc2b9e12f4a8
Andrzejf <afornalik2@gmail.com>
2019-11-11 14:57:54 +0100

    Refactor class TurnStandard.

--- src/main/java/utils/turn/TurnStandard.java
+++ src/main/java/utils/turn/TurnStandard.java
@@ -3,25 +3,44 @@ package utils.turn;
 import entity.Player;
 import entity.Skirmish;
 import entity.Turn;
+import service.skirmish.ISkirmishStandardService;
 import utils.skirmish.SkirmishStandard;
 
-import java.util.Arrays;
-import java.util.Comparator;
+
 
 public class TurnStandard extends Turn {
 
-    public TurnStandard(Player[] playersBeforeTurn) {
-        super(playersBeforeTurn);
+
+
+    public TurnStandard(Player[] playersBeforeTurn, ISkirmishStandardService iSkirmishStandardService) {
+        super(iSkirmishStandardService, playersBeforeTurn);
     }
 
     @Override
     public void doTurn() {
         Skirmish[] skirmishes = new Skirmish[getPlayersBeforeTurn().length];
-        for(int i = 0 ; i < getPlayersBeforeTurn().length; i++){
-            skirmishes[i] = new SkirmishStandard(super.getPlayersBeforeTurn()[i].getPlayerDeckInHand().takeCard(),super.getPlayersBeforeTurn()[i]);
+        for (int i = 0; i < getPlayersBeforeTurn().length; i++) {
+            skirmishes[i] = new SkirmishStandard(
+                    super.getPlayersBeforeTurn()[i]
+                            .getPlayerDeckInHand()
+                            .takeCard()
+                    , super.getPlayersBeforeTurn()[i]);
         }
-        Arrays.stream(skirmishes).map(Skirmish::getCard).max(Comparator.comparingInt(o -> o.getCardType().getCardValue())).get();
+
+        Skirmish skirmish = super.getiSkirmishStandardService().receiveWinner(skirmishes);
+
+        for(int i = 0 ; i < getPlayersBeforeTurn().length; i++){
+            if(skirmishes[i].getPlayer() == skirmish.getPlayer()){
+                System.out.println(skirmish);
+            }
+        }
+
+        //super.setPlayersAfterTurn();
+        super.getPlayersAfterTurn();
+
     }
+
+
 
     @Override
     public Player showWinner() {
